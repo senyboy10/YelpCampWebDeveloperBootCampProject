1938d06f223113f164402c7568b1c0dcb0da563c
senyboy10 <senyboy10@gmail.com>
2019-03-31 12:25:11 -0400

    add some styling using bootstrap

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //Create the express framework
-var express       = require("express"),
-    app           = express();
+var express = require("express"),
+    app = express(),
+    port = 3000,
+    bodyParser = require("body-parser");
 
-const port        = 3000;
 
-var bodyParser 	  = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 //routing for html files. no need for extension (file type)
 app.set("view engine", "ejs");
@@ -14,41 +15,38 @@ app.set("view engine", "ejs");
 
 
 var campgrounds = [
-		{name: "Alseny Sylla", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg"},
-		{name: "Nana Sylla",   image: "https://cdn.pixabay.com/photo/2016/03/30/02/57/camping-1289930__340.jpg"},
-		{name: "Hamidou Sylla",image: "https://cdn.pixabay.com/photo/2013/07/13/11/36/volkswagen-158463_1280.png"}
-]
-//deault landing page
-app.get("/", function(req, res){
-	res.render("landing");
+        { name: "Alseny Sylla", image: "https://images.pexels.com/photos/1376960/pexels-photo-1376960.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+        { name: "Nana Sylla", image: "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+        { name: "Hamidou Sylla", image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" }
+    ]
+    //deault landing page
+app.get("/", function(req, res) {
+    res.render("landing");
 });
 
 
 //  "/campgrounds" will show us all the campground that we have
-app.get("/campgrounds", function(req, res){
-	//Adding temporary campground mode (hardcoding for testing)
-	
-										  //right side the data we are passing in
-										  //left side is the page we are opening. 
-	res.render("campgrounds", {campgrounds:campgrounds});
+app.get("/campgrounds", function(req, res) {
+    //Adding temporary campground mode (hardcoding for testing)
+
+    //right side the data we are passing in
+    //left side is the page we are opening. 
+    res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 // "/newCampground" will allow us to add a new campground
-app.get("/campgrounds/newCampground", function(req, res){
-	res.render("newCampground.ejs");
+app.get("/campgrounds/newCampground", function(req, res) {
+    res.render("newCampground.ejs");
 });
 
-app.post("/campgrounds", function(req, res){
-	var name_  = req.body.name;
-	var image_ = req.body.image;
-	var newCamGround = {name:name_, image:image_}
-	campgrounds.push(newCamGround);
-	res.redirect("/campgrounds");
+app.post("/campgrounds", function(req, res) {
+    var name_ = req.body.name;
+    var image_ = req.body.image;
+    var newCamGround = { name: name_, image: image_ }
+    campgrounds.push(newCamGround);
+    res.redirect("/campgrounds");
 });
 //Listen on port 3000
-app.listen(port, function(){
-	console.log("YelCamp Server has Started!");
+app.listen(port, function() {
+    console.log("YelCamp Server has Started!");
 });
-
-
-
