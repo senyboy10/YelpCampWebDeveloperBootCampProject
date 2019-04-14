9a5a2d29c2fff21417574636a397c79ad3ee96d3
senyboy10 <senyboy10@gmail.com>
2019-04-13 23:51:22 -0400

    removed tes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Create the express framework
 var express = require("express"),
     app = express(),
-    //port = 3000,
+    port = 3000,
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     passport = require("passport"),
@@ -23,6 +23,7 @@ var commentRoutes = require("./routes/comments"),
 //seedDB();
 
 //open connection with mongoDB for the yelcamp DB
+/*
 mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true }, function(err) {
     if (err) {
         console.log(err);
@@ -30,15 +31,15 @@ mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true }
         console.log("successful connection to local mongoDB");
     }
 });
-
-//const uri = "mongodb+srv://senyboy10:12345Alseny@firstcluster-iauqk.mongodb.net/test?retryWrites=true";
-/*mongoose.connect(uri, { useNewUrlParser: true }, function(err) {
+*/
+const uri = "mongodb+srv://senyboy10:12345Alseny@firstcluster-iauqk.mongodb.net/?retryWrites=true";
+mongoose.connect(uri, { useNewUrlParser: true }, function(err) {
     if (err) {
         console.log(err);
     } else {
         console.log("mongoose atlas was successfully connected");
     }
-});*/
+});
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
