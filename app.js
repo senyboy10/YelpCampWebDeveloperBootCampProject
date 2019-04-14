b32385a992658d1978c4e46edab156d7aab00c38
senyboy10 <senyboy10@gmail.com>
2019-04-13 22:33:55 -0400

    connecting heroku to local database

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ var commentRoutes = require("./routes/comments"),
 //seedDB();
 
 //open connection with mongoDB for the yelcamp DB
-//mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
 
-const uri = "mongodb+srv://senyboy10:12345Alseny@firstcluster-iauqk.mongodb.net/test?retryWrites=true";
-mongoose.connect(uri, { useNewUrlParser: true }, function(err) {
+//const uri = "mongodb+srv://senyboy10:12345Alseny@firstcluster-iauqk.mongodb.net/test?retryWrites=true";
+/*mongoose.connect(uri, { useNewUrlParser: true }, function(err) {
     if (err) {
         console.log(err);
     } else {
         console.log("mongoose atlas was successfully connected");
     }
-});
+});*/
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
